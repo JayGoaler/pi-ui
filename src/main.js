043e291e5e7814b61ccb37ac98bb224c5dfb3217
JayGoal <yj936290344@163.com>
2020-12-03 16:29:03 +0800

    解决跨域问题

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ import "element-ui/lib/theme-chalk/index.css";
 Vue.use(VueAxios, axios);
 Vue.use(ElementUI);
 
-axios.defaults.baseURL = "http://localhost:9527";
-
+// axios.defaults.baseURL = "http://localhost:9527";
+axios.defaults.withCredentials = true;
+axios.defaults.timeout = 5000;
 Vue.config.productionTip = false;
 
 new Vue({
