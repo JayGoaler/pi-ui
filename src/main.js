1f2d7544efe5a1435b4ac4493a295fc44e610e49
JayGoal <yj936290344@163.com>
2020-12-03 14:47:42 +0800

    升级结构

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(VueAxios, axios);
+Vue.use(ElementUI);
+
+axios.defaults.baseURL = "http://localhost:9527";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  el: "#app",
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/respberryPi.jpg" />
+    <File msg="Welcome to Your RespberryPi" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import File from "@/components/File.vue";
+
+export default {
+  name: "Home",
+  components: {
+    File
+  }
+};
+</script>
+<style scoped>
+img {
+  width: 20%;
+}
+</style>
