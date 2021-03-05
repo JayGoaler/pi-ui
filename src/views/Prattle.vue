aa1ba6eec521c7391f405634c13bb5b931e75cf1
JayGoal <yj936290344@163.com>
2021-03-05 17:22:11 +0800

    prattle页面

---
+++ src/views/Prattle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="prattle">
+    <h1>{{ prattle }}</h1>
+    <el-button size="small" type="success" round @click="getNext"
+      >下一条</el-button
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    prattle: String
+  },
+  mounted: function() {
+    this.getNext();
+  },
+  methods: {
+    getNext() {
+      this.axios({
+        method: "get",
+        url: "/prattle/get"
+      }).then(async resp => {
+        if (resp.status === 200) {
+          this.prattle = resp.data.data;
+        }
+      });
+    }
+  }
+};
+</script>
